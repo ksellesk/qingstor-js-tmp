0ab59236396a6183edda2206d4eb3c4e3d8fc5fb
daniel <daniel@yunify.com>
2016-05-11 20:05:34 +0800

    Add test_list_objects.js

---
+++ test_list_objects.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var auth = require('./auth');
+
+function SendRequest (method, path, headers, params) {
+    var options = {
+        hostname: 'yourbucket.pek3a.qingstor.com',
+        port: 80,
+        method: method,
+        path: path,
+        headers: headers
+    };
+
+    var req = http.request(options, function(res) {
+        console.log("statusCode: ", res.statusCode);
+        console.log("headers: ", res.headers);
+
+        res.on('data', function(d) {
+            process.stdout.write(d);
+        });
+    });
+
+    req.end();
+
+    req.on('error', function(e) {
+        console.error(e);
+    });
+}
+
+method = "GET";
+authpath = "/yourbucket";
+path = "/"
+headers = {};
+params = {};
+headers["Date"] = new Date().toUTCString();
+
+signature = auth.GenSignature(method, authpath, headers, params);
+authorization = auth.GenAuthorization(signature);
+headers["Authorization"] = authorization;
+console.log(headers);
+SendRequest(method, path, headers, params);
